828bf7a0cc41bb20f1be38965d61d6e7cadbd0c0
Kasun <kasun.kularatne@ajbell.co.uk>
2020-03-11 23:16:35 +0000

    Adding Calculate Price with Offer function to order class

--- Source/CheckoutSystem/CheckoutSystem/Models/Order.cs
+++ Source/CheckoutSystem/CheckoutSystem/Models/Order.cs
@@ -23,5 +23,42 @@ namespace CheckoutSystem.Models
         {
             return OrderLines.Sum(orderLine => orderLine.Product.UnitPrice * orderLine.Quantity);
         }
+
+        public double CalculatePriceWithOffers()
+        {
+            double offerPrice = 0.0;
+            foreach (var orderLine in OrderLines)
+            {
+                if (IsQualifyForMultiBuyOffer(orderLine))
+                {
+                    offerPrice += CalculateUnderOfferPrice(orderLine);
+                    offerPrice += CalculateOverOfferPrice(orderLine);
+                }
+                else
+                {
+                    offerPrice += orderLine.Product.UnitPrice * orderLine.Quantity;
+                }
+            }
+
+            return offerPrice;
+        }
+
+        private static bool IsQualifyForMultiBuyOffer(OrderLine orderLine)
+        {
+            return orderLine.Product.IsMultiBuyOfferAvailable &&
+                   orderLine.Quantity >= orderLine.Product.MultiBuyOffer.Threshold;
+        }
+
+        private static double CalculateUnderOfferPrice(OrderLine orderLine)
+        {
+            var quantity = orderLine.Quantity / orderLine.Product.MultiBuyOffer.Threshold;
+            return orderLine.Product.MultiBuyOffer.Threshold * orderLine.Product.MultiBuyOffer.UnitPrice * quantity;
+        }
+
+        private static double CalculateOverOfferPrice(OrderLine orderLine)
+        {
+            var quantity = orderLine.Quantity % orderLine.Product.MultiBuyOffer.Threshold;
+            return orderLine.Product.UnitPrice * quantity;
+        }
     }
 }
