d42623dcf11d8686b21a6d1c308dfe30a26c585f
Kasun <kasun.kularatne@ajbell.co.uk>
2020-03-12 00:05:56 +0000

    Adding unit tests

--- Test/CheckoutSystemTest/UnitTest1.cs
+++ Test/CheckoutSystemTest/UnitTest1.cs
@@ -26,5 +26,59 @@ namespace CheckoutSystemTest
             // assert
             Assert.Equal(unitPrice * quantity, result);
         }
+
+        [Fact]
+        public void MultiBuyOfferWithOneProductTest()
+        {
+            // arrange
+            var fixture = new Fixture();
+            var sku = fixture.Create<string>();
+            var unitPrice = 1.0;
+            var product = new Product(sku, unitPrice);
+            product.MultiBuyOffer = new MultiBuyOffer(0.5,2);
+            var orderLine = new OrderLine(product,5);
+            var order = new Order();
+            order.AddOrderLine(orderLine);
+
+            // act
+            var totalPrice = order.TotalPrice;
+            var offerPrice = order.CalculatePriceWithOffers();
+
+            // assert
+            Assert.Equal(5.0,totalPrice);
+            Assert.Equal(3.0,offerPrice);
+        }
+
+        [Fact]
+        public void MultiBuyOfferWithMultiProductsTest()
+        {
+            // arrange
+            var fixture = new Fixture();
+            var sku = fixture.Create<string>();
+            var unitPrice = 1.0;
+            var productOne = new Product(sku, unitPrice);
+            productOne.MultiBuyOffer = new MultiBuyOffer(0.5, 2);
+            var orderLineOne = new OrderLine(productOne, 5);
+
+            var skuTwo = fixture.Create<string>();
+            var unitPriceTwo = 2.0;
+            var productTwo = new Product(skuTwo, unitPriceTwo);
+            var orderLineTwo = new OrderLine(productTwo, 5);
+
+            var orderLineThree = new OrderLine(productOne,5);
+
+            var order = new Order();
+            order.AddOrderLine(orderLineOne);
+            order.AddOrderLine(orderLineTwo);
+            order.AddOrderLine(orderLineThree);
+
+            // act
+            var totalPrice = order.TotalPrice;
+            var offerPrice = order.CalculatePriceWithOffers();
+
+            // assert
+            Assert.Equal(20, totalPrice);
+            Assert.Equal(16, offerPrice);
+        }
     }
 }
